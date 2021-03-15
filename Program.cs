1059fac2d9953efa51da8aff15d39dbb0a83d801
Leonardo Gonçalves <leonardo@lginfo.com.br>
2021-03-15 14:10:01 -0300

    estudo C# praticando - criando menu

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+            
+            string opcaoUsuario = Console.ReadLine();
+
+            while(opcaoUsuario.ToUpper() !="x")
+            {
+                switch(opcaoUsuario)
+                
+                {
+                    case ("1"):
+                        //todo: Add aluno
+                    break;
+                    
+                    case ("2"):
+                        //todo: Listar aluno
+                    break;
+
+                    case ("3"):
+                        //todo: Calcular media geral
+                    break;
+                    
+                    default:
+
+                    throw new ArgumentOutOfRangeException();
+
+                }
+
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+
+            }
+        }
+    }
+}
